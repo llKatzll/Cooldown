894356c4a54b7de08437bb79e502930b6310ee3a
llKatzll <bluedia0113@gmail.com>
2023-01-11 23:08:17 +0900

    >:C

--- Cooldown/Cooldown.cpp
+++ Cooldown/Cooldown.cpp
@@ -5,8 +5,13 @@
 
 using namespace std;
 
-string skillname;
-int cooltime;
+string skillname[99];
+string sname;
+int cooltime[99];
+int stime;
+string choose;
+
+
 
 void introduce() {
 	cout << "TRPG 쿨타임 계산기 입니다." << endl;
@@ -14,16 +19,59 @@ void introduce() {
 	cout << endl;
 }
 
-void sayyourskillname() {
-	cout << "스킬 이름을 말해주세요" << endl;
+void choossing() {
+	cout << "무엇을 도와드릴까요?" << endl;
 
-	cin >> name;
+	cout << endl;
+
+	cout << "1 - 쿨다운 1 감소" << endl;
+	cout << "2 - 스킬 등록" << endl;
+	cout << "3 - 현재 스킬 현황" << endl;
+	
+	cout << endl;
+
+	cin >> choose;
+
+	cout << endl;
+
+	if (choose == "1") {
+		cout << "모든 스킬의 쿨다운을 1 낮췄습니다." << endl;
+		cout << endl;
+		cout << "스킬 목록" << endl;
+	}
+
+	if (choose == "2") {
+		cout << "스킬 이름을 말해주세요" << endl;
+
+		cout << endl;
+
+		cin >> sname;
+		skillname[0] = sname;
+
+
+		cout << endl;
+
+		cout << "이제 쿨타임을 말해주세요" << endl;
+
+		cout << endl;
+
+		cin >> stime;
+		cooltime[0] = stime;
+		cout << endl;
+	}
+
+	if (choose == "3") {
+		cout << endl;
+		cout << "스킬 목록" << endl;
+	}
 
 }
-
 int main()
 {
 	introduce();
+	while (true) {
+		choossing();
+	}
 }
 
 
