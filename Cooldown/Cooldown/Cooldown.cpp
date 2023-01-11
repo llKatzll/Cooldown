4c2eb9101151558172f50b4f3e4f3f9270dbb1e4
llKatzll <bluedia0113@gmail.com>
2023-01-11 09:52:23 +0900

    Start

---
+++ Cooldown/Cooldown/Cooldown.cpp
@@ -0,0 +1,13 @@
+﻿// Cooldown.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "TRPG Cooldown 계산기 입니다." << endl;
+}
+
+
