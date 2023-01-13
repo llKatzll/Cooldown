695c465d9e1922b6180683e4811d121d74c59e9a
llKatzll <bluedia0113@gmail.com>
2023-01-13 22:03:12 +0900

    Finally DONE
    perfect

--- Cooldown/Cooldown.cpp
+++ Cooldown/Cooldown.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,70 +13,93 @@ int stime;
 int increase = 0;
 string choose;
 
+struct skill_t {
+	int cooldown;
+	string name;
+};
+
+vector<skill_t> skill;
 
 
 void introduce() {
-	cout << "TRPG 쿨타임 계산기 입니다." << endl;
+	cout << "$$$  TRPG 쿨타임 계산기 입니다.  $$$" << endl;
 
 	cout << endl;
 }
 
 void choossing() {
+	cout << "========================================" << endl;
+
 	cout << "무엇을 도와드릴까요?" << endl;
 
-	cout << endl;
+	cout << "========================================" << endl;
 
 	cout << "1 - 쿨다운 1 감소" << endl;
 	cout << "2 - 스킬 등록" << endl;
 	cout << "3 - 현재 스킬 현황" << endl;
 	
-	cout << endl;
+	cout << "========================================" << endl;
 
 	cin >> choose;
 
 	cout << endl;
 
 	if (choose == "1") {
+		cout << "========================================" << endl;
 		cout << "모든 스킬의 쿨다운을 1 낮췄습니다." << endl;
-		cout << endl;
-		cout << "스킬 목록" << endl;
-	}
+		cout << "========================================" << endl;
+		for (int l = 0; l < skill.size(); l++) {
 
-	if (choose == "2") {
-		cout << "스킬 이름을 말해주세요" << endl;
+			skill[l].cooldown--;
 
-		cout << endl;
-
-		cin >> sname;
-		skillname[increase] = sname;
-		increase++;
-
-
-		cout << endl;
-
-		cout << "이제 쿨타임을 말해주세요" << endl;
-
-		cout << endl;
-
-		cin >> stime;
-		cooltime[0] = stime;
-		cout << endl;
-	}
-
-	if (choose == "3") {
-		cout << endl;
-		cout << "스킬 목록" << endl;
-		for (int i = 0; i < 99; i++) {
-			if (skillname[i] != null) {
-				cout << skillname[i] << endl;
-			}
-			if (skillname[i] == null) {
-				break;
+			if (skill[l].cooldown <= 0) {
+				cout << skill[l].name << " 스킬을 사용할 수 있게 되었습니다!" << endl;
+				skill.erase(skill.begin() + l);
 			}
 		}
+		cout << "========================================" << endl;
+		cout << "스킬 목록" << endl;
+		cout << endl;
+		for (int o = 0; o < skill.size(); o++) {
+			cout << skill[o].name << " -- " << skill[o].cooldown << endl;
+		}
+		cout << "========================================" << endl;
+
+		//skill.erase(skill.begin()+2);
+	}
+
+	else if (choose == "2") {
+		cout << "========================================" << endl;
+		cout << "스킬 이름을 말해주세요" << endl;
+		cout << "========================================" << endl;
+
+		cin >> sname;
+		skill.resize(skill.size() + 1);
+		skill[skill.size() - 1].name = sname;
+
+		cout << "========================================" << endl;
+		cout << "이제 쿨타임을 말해주세요" << endl;
+		cout << "========================================" << endl;
+
+		cin >> stime;
+		skill[skill.size() - 1].cooldown = stime;
+
+		cout << endl;
+	}
+
+	else if (choose == "3") {
+		cout << "========================================" << endl;
+		cout << "스킬 목록" << endl;
+		cout << endl;
+		for (int i = 0; i < skill.size(); i++) {
+			cout << skill[i].name << " -- " << skill[i].cooldown << endl;
+		}
+		cout << "========================================" << endl;
 	}
 
 }
+
+
 int main()
 {
 	introduce();
